20dc1f49e17db206655556be35d522089657a0c5
rodmoten <rodmoten@gmail.com>
2022-09-08 22:58:17 -0400

    Implemented moving with encoder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AroundTheRoom.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AroundTheRoom.java
@@ -40,15 +40,15 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
  * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
  * class is instantiated on the Robot Controller and executed.
- *
+ * <p>
  * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
  * It includes all the skeletal structure that all linear OpModes contain.
- *
+ * <p>
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@Autonomous(name="AroundTheRoom")
+@Autonomous(name = "AroundTheRoom")
 public class AroundTheRoom extends LinearOpMode {
 
     // Declare OpMode members.
@@ -61,48 +61,29 @@ public class AroundTheRoom extends LinearOpMode {
     public AroundTheRoom() {
     }
 
-    private
-    void pauseRobot(long n){
-        try {
-            Thread.sleep(n);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    // Calculate the COUNTS_PER_INCH for your specific drive train.
+    // Go to your motor vendor website to determine your motor's COUNTS_PER_MOTOR_REV
+    // For external drive gearing, set DRIVE_GEAR_REDUCTION as needed.
+    // For example, use a value of 2.0 for a 12-tooth spur gear driving a 24-tooth spur gear.
+    // This is gearing DOWN for less speed and more torque.
+    // For gearing UP, use a gear ratio less than 1.0. Note this will affect the direction of wheel rotation.
+    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // No External Gearing.
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double     DRIVE_SPEED             = 0.6;
+    static final double     TURN_SPEED              = 0.5;
+
+
+    private void stopMotion() {
+        LR.setPower(0);
+        RR.setPower(0);
+        LF.setPower(0);
+        RF.setPower(0);
     }
 
-    private
-    void setWheelPower(double left, double right, long wait){
-        this.RF.setPower(right);
-        this.LF.setPower(left);
-        this.RR.setPower(right);
-        this.LR.setPower(left);
-        this.pauseRobot(wait);
-
-    }
-    private
-    void turn90(){
-        // 90 degrees
-        this.setWheelPower(0.5, 0.5, 600);
-        this.stopMovement();
-    }
-
-    private
-    void moveForward(long n){// forward
-        this.setWheelPower(0.5, -0.5, n);
-        this.stopMovement();
-    }
-
-    private
-    void stopMovement(){
-        this.setWheelPower(0,0,1000);
-    }
-
-    @Override
-    public void runOpMode() {
-        LF = hardwareMap.get(DcMotor.class, "LF");
-        LR = hardwareMap.get(DcMotor.class, "LR");
-        RF = hardwareMap.get(DcMotor.class, "RF");
-        RR = hardwareMap.get(DcMotor.class, "RR");
+    private void resetEncoder() {
         LR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         RR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         LF.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -111,35 +92,117 @@ public class AroundTheRoom extends LinearOpMode {
         RR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         LF.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         RF.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        telemetry.addData("Status", "Initialized");
+    }
+
+    private void currentPosition() {
+        // Send telemetry message to indicate successful Encoder reset
+        telemetry.addData("Starting at", "%7d :%7d",
+                LR.getCurrentPosition(),
+                RF.getCurrentPosition());
         telemetry.update();
+    }
+
+
+    @Override
+    public void runOpMode() {
+        LF = hardwareMap.get(DcMotor.class, "LF");
+        LR = hardwareMap.get(DcMotor.class, "LR");
+        RF = hardwareMap.get(DcMotor.class, "RF");
+        RR = hardwareMap.get(DcMotor.class, "RR");
+        resetEncoder();
+        currentPosition();
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
         runtime.reset();
 
-        // run until the end of the match (driver presses STOP)
+        // Note: Reverse movement is obtained by setting a negative distance (not speed)
+        encoderDrive(DRIVE_SPEED,  48,  48, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
+        encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
+        encoderDrive(DRIVE_SPEED, -24, -24, 4.0);  // S3: Reverse 24 Inches with 4 Sec timeout
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+        sleep(1000);  // pause to display final telemetry message.
+
+    }
+
+    /*
+     *  Method to perform a relative move, based on encoder counts.
+     *  Encoders are not reset as the move is based on the current position.
+     *  Move will stop if any of three conditions occur:
+     *  1) Move gets to the desired position
+     *  2) Move runs out of time
+     *  3) Driver stops the opmode running.
+     */
+    public void encoderDrive(double speed,
+                             double leftInches, double rightInches,
+                             double timeoutS) {
+
+
+        // Ensure that the opmode is still active
         if (opModeIsActive()) {
-            long distance = 2 * 1000;
-            this.stopMovement();
-            this.turn90();
-            this.moveForward(distance);
-            this.turn90();
-            this.moveForward(distance);
-            this.turn90();
-            this.moveForward(distance);
-            this.stopMovement();
 
+            // Determine new target position, and pass to motor controller
+            int newLeftFrontTarget = LF.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+            int newRightFrontTarget = RF.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
+            int newLeftRearTarget = LR.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+            int newRightRearTarget = RR.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+            LF.setTargetPosition(newLeftFrontTarget);
+            RF.setTargetPosition(newRightFrontTarget);
+            LR.setTargetPosition(newLeftRearTarget);
+            RR.setTargetPosition(newRightRearTarget);
 
-            while (opModeIsActive()) {
-                //loop
+            // Turn On RUN_TO_POSITION
+            LF.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            RF.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            LR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            RR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
+            // reset the timeout time and start motion.
+            runtime.reset();
+            LF.setPower(Math.abs(speed));
+            RF.setPower(Math.abs(speed));
+            LR.setPower(Math.abs(speed));
+            RR.setPower(Math.abs(speed));
 
-                telemetry.addData("Status", "Run Time: " + runtime.toString());
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+            // its target position, the motion will stop.  This is "safer" in the event that the robot will
+            // always end the motion as soon as possible.
+            // However, if you require that BOTH motors have finished their moves before the robot continues
+            // onto the next step, use (isBusy() || isBusy()) in the loop test.
+            telemetry.addData("Running to",  " %7d :%7d", newLeftFrontTarget,  newRightFrontTarget);
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    reachedTarget()) {
+
+                // Display it for the driver.
+                currentPosition();
                 telemetry.update();
             }
+
+            // Stop all motion;
+            stopMotion();
+
+            // Turn off RUN_TO_POSITION
+            turnOff();
+            sleep(250);   // optional pause after each move.
         }
 
-        }
+
     }
+
+    private void turnOff() {
+        LF.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        RF.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        LR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        RR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    private boolean reachedTarget(){
+        return (LF.isBusy() && RF.isBusy()) || (LR.isBusy() && RR.isBusy());
+    }
+
+}
 
